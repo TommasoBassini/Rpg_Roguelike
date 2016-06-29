4c88217b732dbfd9e750540869892c8a1d3d4be3
dbga_34 <dbga_34@dbga.local>
2016-06-29 12:35:22 +0200

    Gioco Base

---
+++ UnityProject/Rpg_Roguelike/Assets/Script/Grid.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    public Cell[,] cells = new Cell[20, 20];
+    public GameObject cellPrefab;
+
+    void Start ()
+    {
+        for (int i = 0; i < cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < cells.GetLength(1); j++)
+            {
+                GameObject newCellGo = Instantiate(cellPrefab);
+
+                newCellGo.transform.position = new Vector3(j, i, 0);
+                newCellGo.name = "Cell " + i + " " + j;
+                cells[i, j] = newCellGo.GetComponent<Cell>();
+            }
+        }
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
