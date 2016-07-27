10bb9865fcf120262ecf25954a66d0eb62f48c4b
dbga_27 <dbga_27@dbga.local>
2016-07-27 14:50:36 +0200

    Aggiornate porte, scrigni. Inizio animazioni abilità

--- UnityProject/Rpg_Roguelike/Assets/Script/Esplorazione/TreasureChestManager.cs
+++ UnityProject/Rpg_Roguelike/Assets/Script/Esplorazione/TreasureChestManager.cs
@@ -10,22 +10,24 @@ public class TreasureChestManager : MonoBehaviour
     public bool magePowerUp;
     public bool isClosed = true;
     public bool inFront = false;
+    public Sprite openChest;
+    private SpriteRenderer sr;
 
-	void Start()
+    void Start()
     {
         Invoke("SetObjectToCell", 0.3f);
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();
         sr.color = Color.clear;
     }
 
     void Update()
     {
-        if (inFront == true && Input.GetKey(KeyCode.E)) 
+        if (inFront == true && Input.GetKeyDown(KeyCode.E) && isClosed == true) 
         {
             Vector2 pos = new Vector2(this.transform.position.x - 0.5f, this.transform.position.y - 0.5f);
             Grid grid = FindObjectOfType<Grid>();
             grid.cells[(int)pos.x, (int)pos.y].GetComponent<Cell>().cellObject = this.gameObject;
-            grid.cells[(int)pos.x, (int)pos.y].GetComponent<Cell>().isSemiWall = false;
+            grid.cells[(int)pos.x, (int)pos.y].GetComponent<Cell>().isSemiWall = true;
             isClosed = false;
             if (hpPotion)
             {
@@ -42,7 +44,8 @@ public class TreasureChestManager : MonoBehaviour
                 ui.AggiornaPotion();
             }
             Debug.Log("Scrigno aperto!!");
-            Destroy(this.gameObject);
+            sr.sprite = openChest;
+            
         }
     }
 
