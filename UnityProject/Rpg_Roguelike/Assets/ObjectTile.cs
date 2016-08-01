3a4e6a855cd298b4e11966d598941cf6a6cc8cf0
dbga_69 <dbga_69@dbga.local>
2016-08-01 18:09:51 +0200

    Mappa Completa

---
+++ UnityProject/Rpg_Roguelike/Assets/ObjectTile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectTile : MonoBehaviour
+{
+
+    void Start()
+    {
+        Invoke("SetObjectToCell", 0.3f);
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.color = Color.clear;
+    }
+
+    void SetObjectToCell()
+    {
+        Vector2 pos = new Vector2(this.transform.position.x - 0.5f, this.transform.position.y - 0.5f);
+        Grid grid = FindObjectOfType<Grid>();
+        grid.cells[(int)pos.x, (int)pos.y].GetComponent<Cell>().cellObject = this.gameObject;
+    }
+
+}
